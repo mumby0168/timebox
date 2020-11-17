5e0f05c80759760ef7c4cbec1b736ffc872b19ca
William Mumby <williammumby@Williams-MacBook-Pro.local>
2020-11-17 21:55:22 +0000

    added boiler plate backlog code

---
+++ src/backend/Modules/Backlog/Timebox.Backlog.Api/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Timebox.Backlog.Api
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddBacklogModule(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IApplicationBuilder UseBacklogModule(this IApplicationBuilder app)
+        {
+            return app;
+        }
+    }
+}
--- src/backend/Timebox.Api/Startup.cs
+++ src/backend/Timebox.Api/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Timebox.Backlog.Api;
 using Timebox.Modules.Events;
 using Timebox.Sample.Api;
 
@@ -17,6 +18,7 @@ namespace Timebox.Api
         {
             services.AddDomainEvents();
             services.AddSampleModule();
+            services.AddBacklogModule();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,6 +30,7 @@ namespace Timebox.Api
             }
 
             app.UseSampleModule();
+            app.UseBacklogModule();
             
             app.UseRouting();
 
